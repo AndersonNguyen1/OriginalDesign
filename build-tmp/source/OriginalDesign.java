8c956b90d75e375ec9e09e449811e7d53745934d
unknown <annguyen1@697-334-Khamul.sf.k12.internal>
2014-08-29 11:39:28 -0700

    Added first name to index.html

--- build-tmp/source/OriginalDesign.java
+++ build-tmp/source/OriginalDesign.java
@@ -14,56 +14,93 @@ import java.io.IOException;
 
 public class OriginalDesign extends PApplet {
 
-int xCanvas = 500;                     //size of canvas
-int yCanvas = 500;					   //size of canvas
+int xCanvas = 500;                     // size of canvas
+int yCanvas = 500;                     // size of canvas
 
-int yCookieLocation = 0;
+int[] xCookieLocation = new int[10];               // X value of cookies 
+int[] yCookieLocation = new int[10];               // Y value of cookies
 
-int cookieSize = 70;
+int cookieSize = 70;                   // Cookie Size
+
+boolean  mouseClicked = false;         // to let the word Click turn into Cookies
+
+PImage cookieMonster;
 
 public void setup()
 {
     size(xCanvas, yCanvas);
+    cookieMonster = loadImage("cookieMonster.png");
+    randomizedXPosition();
+    randomizedYPosition();
 }
 
 public void draw()
 {
-    drawMonsterFace();
+    background(0, 255, 0);
     drawText();
     drawCookies();
+    image(cookieMonster, -95, 120);
+    mouseClicking();
 }
 
-public void mousePressed()
+public void randomizedXPosition()
 {
-	background(255);
-	fill(0, 0, 255);
-	textAlign(CENTER);
-	textSize(60);
-	text("Coooookieees!", xCanvas/2, yCanvas/2 - 100);
-	drawCookies();  
-
+    for(int i = 0; i < xCookieLocation.length; ++i)
+    {
+        xCookieLocation[i] = (int)(Math.random()* xCanvas);
+    }
 }
 
-public void drawMonsterFace()
+public void randomizedYPosition()
 {
-	fill(0, 0, 255);
-	stroke(0, 0, 255);
-    ellipse(xCanvas/2, yCanvas/2, 100, 100);
-}
-
-public void drawText()
-{
-	textAlign(CENTER);
-	textSize(60);
-	text("Click Me!", xCanvas/2, yCanvas/2 - 100);
+    for(int i = 0; i < xCookieLocation.length; ++i)
+    {
+        yCookieLocation[i] = (int)(Math.random()* - 1500);
+    }
 }
 
 public void drawCookies()
 {
-	stroke(250, 69, 19);
-	fill(139, 69, 19);
-	ellipse(random(0,500), yCookieLocation, cookieSize, cookieSize);
-	yCookieLocation += 5;
+    for(int i = 0; i < xCookieLocation.length; ++i)
+    {
+        stroke(250, 69, 19);
+        fill(139, 69, 19);
+        ellipse(xCookieLocation[i], yCookieLocation[i], cookieSize, cookieSize);
+        yCookieLocation[i] += 10;
+        if (yCookieLocation[i] > yCanvas + cookieSize/2+ 300)
+        {
+            xCookieLocation[i] = (int)(Math.random()* xCanvas);
+            yCookieLocation[i] = (int)(Math.random()* - 1500);
+            yCookieLocation[i] = 0;            
+        }   
+    }
+}
+
+public void mouseClicking()
+{
+    if (mouseClicked)
+    {
+        background(0, 255, 0);
+        fill(0, 0, 255);
+        textAlign(CENTER);
+        textSize(60);
+        text("Coooookieees!", xCanvas/2, yCanvas/2 - 150);
+        image(cookieMonster, -95, 120);
+        drawCookies();
+    }
+}   
+
+public void mouseClicked()
+{
+    mouseClicked = true;
+}   
+
+public void drawText()
+{
+	fill(0, 0, 255);
+    textAlign(CENTER);
+    textSize(60);
+    text("Click Me!", xCanvas/2, yCanvas/2 - 150);
 }
 
   static public void main(String[] passedArgs) {
